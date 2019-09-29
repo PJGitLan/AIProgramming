c2ef9e0a3d09b5eccdf113336c27d097b32620fc
Pieter-Jan Lanneer <pj.lanneer@gmail.com>
2019-09-29 17:01:06 +0200

    gitignore added

---
+++ introOef5/Sensor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace introOef5
+{
+    class Sensor
+    {
+        public int refreshRate { get; set; } = 1;
+        public bool valAvailable { get; set; } = false;
+
+        private String distanceIn;
+        public String DistanceIn
+        {
+            get { return distanceIn; }
+            set { distanceIn = Console.ReadLine(); }
+        }
+
+    }
+}
---
+++ introOef5/car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace introOef5
+{
+    class car
+    {
+        private float distance;
+
+        public car(float _distance)
+        {
+            float distance = _distance;
+        }
+        public float PrevDistance { get; set; }
+        public float RelVelocity{ get; set; }
+        public float CollisionTime { get; set; }
+        public bool ValAvailable { get; set; }
+
+        private void Brake()
+        {
+            Console.WriteLine("Brake");
+        }
+
+        private void saveDistance()
+        {
+            PrevDistance = distance;
+            ValAvailable = true;
+        }
+    }
+}
